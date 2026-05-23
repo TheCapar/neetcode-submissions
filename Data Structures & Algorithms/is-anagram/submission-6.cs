2f4863fc230346da201024d61779b403bb7598b4
Deha Edebali Çapar <52006028+TheCapar@users.noreply.github.com>
2026-05-23 20:05:18 +0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length || s == null || t == null) return false;
+        for(int i = 0; i<s.Length; i++){
+            bool a = s.Contains(t[i]);
+            if(a == false) return false;
+            return true;
+        }
+        string chars = s + s;
+        return chars.Contains(t);
+    }
+}
